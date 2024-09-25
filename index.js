f323a595de83ee37736e440832d3c1ca866d43d0
Muhammad Taha <tahaqureshi203040@gmail.com>
2024-09-26 01:51:34 +0500

    add configu file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-
 const counter = () => {
     let count = 0;
-    for (let  i = 0; i <= 5; i++) {
+    for (let i = 0; i <= 5; i++) {
         console.log(count);
+        count++; // ESLint might flag this for being unused or misplaced
     }
 }
 
